8368a658d144a8c0df3b599352bc040fbf118bcc
leiseek <leiseek@users.noreply.github.com>
2026-06-25 02:34:27 +0000

    deploy: 9cce018401309641a20dddc0d9566dcd15462764

---
+++ pr-38/dist/game-core/index.js
@@ -0,0 +1,5 @@
+export { level001Config, tutorialLevel } from "./level.js";
+export { nextRandom, normalizeSeed } from "./random.js";
+export { createInitialGameState, createSnapshot, enqueueAction, restoreSnapshot } from "./state.js";
+export { reduceActions, stepFixedTick, stepSimulation } from "./simulation.js";
+export { HUD_SPEED_OPTIONS, selectHudState } from "./hud.js";
